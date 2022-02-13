ac528e3905d05bfce22269a3692777a2b09311da
Clayrisee <ardikatamah@gmail.com>
2022-02-13 13:56:36 +0700

    add convnext and cdcnext backbone

---
+++ Trainer/models/models.py
@@ -0,0 +1,69 @@
+import torch
+from backbones_models import *
+
+
+
+model_urls = {
+    "convnext_tiny_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth",
+    "convnext_small_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_small_1k_224_ema.pth",
+    "convnext_base_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_1k_224_ema.pth",
+    "convnext_large_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_1k_224_ema.pth",
+    "convnext_base_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_base_22k_224.pth",
+    "convnext_large_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_large_22k_224.pth",
+    "convnext_xlarge_22k": "https://dl.fbaipublicfiles.com/convnext/convnext_xlarge_22k_224.pth",
+}
+
+def convnext_tiny(pretrained=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_tiny_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+def convnext_small(pretrained=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        url = model_urls['convnext_small_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+def convnext_base(**kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024], **kwargs)
+    return model
+
+def convnext_large(**kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], **kwargs)
+    return model
+
+# Function to generate CDCNeXt
+
+def get_cdcnext(model_type="base", **kwargs):
+    models = {
+        "tiny": CDCNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs),
+        "small":CDCNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs),
+        "base": CDCNeXt(depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024], **kwargs),
+        "large": CDCNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], **kwargs)
+    }
+
+    return models[model_type]
+
+# def cdcnext_tiny(**kwargs):
+#     model = CDCNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+#     return model
+
+
+# def cdcnext_small(**kwargs):
+#     model = CDCNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
+#     return model
+
+
+# def cdcnext_base(**kwargs):
+#     model = CDCNeXt(depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024], **kwargs)
+#     return model
+
+# def cdcnext_large(**kwargs):
+#     model = CDCNeXt(depths=[3, 3, 27, 3], dims=[192, 384, 768, 1536], **kwargs)
+#     return model
