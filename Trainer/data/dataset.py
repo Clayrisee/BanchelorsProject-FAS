0f60cd19fa1c9659ccf2295a25a82fca83971bd8
Clayrisee <ardikatamah@gmail.com>
2022-02-14 18:05:15 +0700

    update Dataset using pytorch Dataset

--- Trainer/data/dataset.py
+++ Trainer/data/dataset.py
@@ -2,37 +2,36 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import pandas as pd
+from torch.utils.data import Dataset
+import os
 
-class PixWiseDataset():
-    def __init__(self, csvfile, map_size=7,
-                 smoothing=True, transform=None):
-        self.data = pd.read_csv(csvfile)
-        self.transform = transform
+class FASDataset(Dataset):
+    def __init__(self, root_dir, csv_file, map_size=7, transform=None, smoothing=True):
+        super().__init__()
+        self.root_dir = root_dir
+        self.data = pd.read_csv(os.path.join(self.root_dir, csv_file))
         self.map_size = map_size
-        self.label_weight = 0.99 if smoothing else 1.0
+        self.transform = transform
+        
+        if smoothing:
+            self.label_weight = 1.0
+        else:
+            self.label_weight = 0.99
+    
+    def __getitem__(self, index) :
+        img_name = self.data.iloc[index, 0]
+        img_name = os.path.join(self.root_dir, img_name)
 
-    def dataset(self):
-        images = []
-        labels = []
-        masks = []
+        img = Image.open(img_name)
+        label = self.data.iloc[index, 1].astype(np.float32)
+        label = np.expand_dims(label, axis=0)
 
-        for ind in self.data.index:
-            img_name = self.data.iloc[ind]['path']
-            img = Image.open(img_name)
-            label = self.data.iloc[ind]['target']
-            if label == 0:
-                mask = np.ones((1, self.map_size, self.map_size), dtype=np.float32) * (1 - self.label_weight)
-            else:
-                mask = np.ones((1, self.map_size, self.map_size), dtype=np.float32) * (self.label_weight)
-
-            if self.transform:
-                img = self.transform(img)
-
-            images.append(img)
-            labels.append(label)
-            masks.append(mask)
-
-        labels = np.array(labels, dtype=np.float32)
-
-        dataset = [[images[i], masks[i], labels[i]] for i in range(len(images))]
-        return dataset
+        if label == 1:
+            mask = np.ones((1, self.map_size, self.map_size), dtype=np.float32) * (1 - self.label_weight)
+        else:
+            mask = np.ones((1, self.map_size, self.map_size), dtype=np.float32) * (self.label_weight)
+        
+        if self.transform:
+            img = self.transform(img)
+        
+        return img, mask, label
