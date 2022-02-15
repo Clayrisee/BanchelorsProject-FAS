00f34c4986de40cbbe0fed32e2f61753ba3bbb56
Clayrisee <ardikatamah@gmail.com>
2022-02-15 10:11:58 +0700

    update train one epoch, add val_one_epoch, and add train function

--- Trainer/trainer/FASTrainer.py
+++ Trainer/trainer/FASTrainer.py
@@ -1,13 +1,12 @@
 import os
 import torch
-import torchvision
 from trainer.base import BaseTrainer
 from utils.metrics import calculate_liveness_metric
 from utils.utils import scoring_method
 
 
 class FASTrainer(BaseTrainer):
-    def __init__(self, cfg, network, optimizer, criterion, dataset, lr_scheduler, device, logger):
+    def __init__(self, cfg, network, optimizer, criterion, dataset, device, lr_scheduler=None, logger=None):
         self.cfg = cfg
         self.network = network
         self.optimizer = optimizer
@@ -42,7 +41,7 @@ class FASTrainer(BaseTrainer):
         self.network.train()
         self.train_loss_metric.reset(epoch)
         self.train_acc_metric.reset(epoch)
-
+        loss = 0
         for i, (imgs, masks, labels) in enumerate(self.trainloader):
             imgs, masks, labels = imgs.to(self.device), masks.to(self.device), labels.to(self.device)
             pred_mask, preds = self.network(imgs)
@@ -50,21 +49,52 @@ class FASTrainer(BaseTrainer):
             loss = self.criterion(pred_mask, preds, masks, labels)
             loss.backward()
             self.optimizer.step()
+            loss += loss.item() * imgs.shape[0]
+            # TODO: add logger to record loss for each step.
 
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step()
+        
+        epoch_loss = loss / len(self.dataset.train_set)
+        if self.logger is not None:
+            self.logger.log_metric("train_loss", epoch_loss, epoch=epoch)
 
-
-            # Update metrics
-            # self.train_loss_metric.update(loss.item())
-            # self.train_acc_metric.update(accuracy)
-
-            # print('Epoch: {}, iter: {}, loss: {}, acc: {}'.format(epoch, epoch * len(self.trainloader) + i, self.train_loss_metric.avg, self.train_acc_metric.avg))
-
+        return epoch_loss
     
     def train(self):
-        pass
+        for epoch in range(self.cfg['train']['num_epochs']):
+            print("="*80)
+            print("Epoch: {}".format(epoch))
+            train_loss = self.train_one_epoch(epoch)
+            val_apcer, val_npcer, val_acer, val_loss = self.validate_one_epoch(epoch)
+            print("Train Loss: {:.4f}, Val Loss: {:.4f}\n APCER: {:.4f}, NPCER: {:.4f}, ACER: {:.4f}"\
+                .format(train_loss,val_loss, val_apcer, val_npcer, val_acer))
+            print("="*80)
     
-    def validate(self):
-        pass
+    def validate_one_epoch(self, epoch):
+        apcer = npcer = acer = val_loss = 0
+        self.network.eval()
+        with torch.no_grad():
+            for i, (imgs, masks, labels) in enumerate(self.valloader):
             # # TODO: add scoring method
-        # pred_scores = scoring_method(pred_mask, preds)
-        # liveness_metrics = self.eval_metrics(pred_scores, labels)
+                imgs, masks, labels = imgs.to(self.device), masks.to(self.device), labels.to(self.device)
+                pred_mask, preds = self.network(imgs)
+                loss = self.criterion(pred_mask, preds, masks, labels)
+                pred_scores = scoring_method(pred_mask, preds)
+                liveness_metrics = self.eval_metrics(pred_scores, labels)
+                val_loss += loss.item() * imgs.shape[0]
+                apcer += liveness_metrics['apcer']
+                npcer += liveness_metrics['npcer']
+                acer += liveness_metrics['acer']
+                # TODO: add logger to record result metric for each step.
+            epoch_val_loss = loss / len(self.dataset.val_set)
+            epoch_apcer = apcer / len(self.dataset.val_set)
+            epoch_npcer = npcer / len(self.dataset.val_set)
+            epoch_acer = acer / len(self.dataset.val_set)
+            
+            if self.logger is not None:
+                self.logger.log_metric("epoch_val_loss", epoch_val_loss, epoch=epoch)
+                self.logger.log_metric("epoch_apcer", epoch_apcer, epoch=epoch)
+                self.logger.log_metric("epoch_npcer", epoch_npcer, epoch=epoch)
+                self.logger.log_metric("epoch_acer", epoch_acer, epoch=epoch)
+        return epoch_apcer, epoch_npcer, epoch_acer, epoch_val_loss
