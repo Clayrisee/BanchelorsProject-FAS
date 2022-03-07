2c900808bed08153a862d5f9f00f0bb68b4fea33
Clayrisee <ardikatamah@gmail.com>
2022-03-07 17:08:59 +0700

    add outmap result at forward function

--- Trainer/models/convnext.py
+++ Trainer/models/convnext.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from timm.models.layers import trunc_normal_
 
+
 class ConvNeXt(nn.Module):
     r""" ConvNeXt
         A PyTorch impl of : `A ConvNet for the 2020s`  -
@@ -24,10 +25,10 @@ class ConvNeXt(nn.Module):
         super().__init__()
 
         self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
-        base_conv = Conv2d_cd if conv_type=="cdc" else nn.Conv2d
+        self.base_conv = Conv2d_cd if conv_type=="cdc" else nn.Conv2d
         
         stem = nn.Sequential(
-            base_conv(in_chans, dims[0], kernel_size=4, stride=4),
+            self.base_conv(in_chans, dims[0], kernel_size=4, stride=4),
             LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
         )
         self.downsample_layers.append(stem)
@@ -44,7 +45,7 @@ class ConvNeXt(nn.Module):
         
         for i in range(4):
             stage = nn.Sequential(
-                *[Block(dim=dims[i],base_conv=base_conv, drop_path=dp_rates[cur + j], 
+                *[Block(dim=dims[i],base_conv=self.base_conv, drop_path=dp_rates[cur + j], 
                 layer_scale_init_value=layer_scale_init_value) for j in range(depths[i],)],
             
             )
@@ -53,13 +54,13 @@ class ConvNeXt(nn.Module):
 
         self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
         self.head = nn.Linear(dims[-1], num_classes)
-
+        self.dec = self.base_conv
         self.apply(self._init_weights)
         self.head.weight.data.mul_(head_init_scale)
         self.head.bias.data.mul_(head_init_scale)
 
     def _init_weights(self, m):
-        if isinstance(m, (nn.Conv2d, nn.Linear)):
+        if isinstance(m, (self.base_conv, nn.Linear)):
             trunc_normal_(m.weight, std=.02)
             nn.init.constant_(m.bias, 0)
 
@@ -67,9 +68,14 @@ class ConvNeXt(nn.Module):
         for i in range(4):
             x = self.downsample_layers[i](x)
             x = self.stages[i](x)
-        return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+        embedding = self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+        return  x, embedding
 
     def forward(self, x):
-        x = self.forward_features(x)
-        x = self.head(x)
-        return x
+        outmap, embedding = self.forward_features(x) # forward feature to feature extraction
+        in_c, out_c = (outmap.shape[1], 1) # get input ch and output ch for decryptor
+        self.dec = self.dec(in_c, out_c) # define decryptor layer
+        outmap = self.dec(outmap) # return outmap for pixelwise supervision
+        x = self.head(embedding) # MLP for classifier
+        x = torch.flatten(x) # return final label
+        return outmap, x
