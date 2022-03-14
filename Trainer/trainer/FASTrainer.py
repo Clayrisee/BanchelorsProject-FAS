6a16819a83de2247b6cae5d105131526dd6050ba
Clayrisee <ardikatamah@gmail.com>
2022-03-14 19:03:00 +0700

    Create save_final_model function and add callback in Trainer

--- Trainer/trainer/FASTrainer.py
+++ Trainer/trainer/FASTrainer.py
@@ -6,7 +6,7 @@ from utils.utils import scoring_method
 
 
 class FASTrainer(BaseTrainer):
-    def __init__(self, cfg, network, optimizer, criterion, dataset, device, lr_scheduler=None, logger=None):
+    def __init__(self, cfg, network, optimizer, criterion, dataset, device, callbacks=None, lr_scheduler=None, logger=None):
         self.cfg = cfg
         self.network = network
         self.optimizer = optimizer
@@ -16,6 +16,7 @@ class FASTrainer(BaseTrainer):
         self.logger = logger
         self.eval_metrics = calculate_liveness_metric
         self.dataset = dataset
+        self.callbacks = callbacks
         self.init_dataloader(dataset=dataset)
 
     def init_dataloader(self, dataset):
@@ -28,19 +29,28 @@ class FASTrainer(BaseTrainer):
         self.optimizer.load_state_dict(state['optimizer'])
         self.network.load_state_dict(state['state_dict'])
     
-    def save_model(self, epoch):
+    # def save_model(self, epoch):
+    #     if not os.path.exists(self.cfg['output_dir']):
+    #         os.makedirs(self.cfg['output_dir'])
+
+    #     saved_name = os.path.join(self.cfg['output_dir'], '{}_{}.pth'.format(self.cfg['model']['base'], self.cfg['dataset']['name']))
+
+    #     state = {
+    #         'epoch': epoch,
+    #         'state_dict': self.network.state_dict(),
+    #         'optimizer': self.optimizer.state_dict()
+    #     }
+        
+    #     torch.save(state, saved_name)
+
+    def save_final_model(self):
         if not os.path.exists(self.cfg['output_dir']):
             os.makedirs(self.cfg['output_dir'])
-
-        saved_name = os.path.join(self.cfg['output_dir'], '{}_{}.pth'.format(self.cfg['model']['base'], self.cfg['dataset']['name']))
-
-        state = {
-            'epoch': epoch,
-            'state_dict': self.network.state_dict(),
-            'optimizer': self.optimizer.state_dict()
-        }
         
-        torch.save(state, saved_name)
+        model_saved_name = os.path.join(self.cfg['output_dir'], 'final_model.pth')
+        optimizer_saved_name = os.path.join(self.cfg['output_dir'], 'final_optimizer.pth')
+        torch.save(self.network.state_dict(), model_saved_name)
+        torch.save(self.optimizer.state_dict(), optimizer_saved_name)
     
     def train_one_epoch(self, epoch):
         self.network.train()
@@ -73,6 +83,19 @@ class FASTrainer(BaseTrainer):
             print("Train Loss: {:.4f}, Val Loss: {:.4f}\n APCER: {:.4f}, NPCER: {:.4f}, ACER: {:.4f}"\
                 .format(train_loss,val_loss, val_apcer, val_npcer, val_acer))
             print("="*80)
+            if self.callbacks is not None:
+                result_val_metris = {
+                    'apcer':val_apcer,
+                    'npcer':val_npcer,
+                    'acer': val_acer
+                }
+                self.callbacks(self.network, self.optimizer, result_val_metris)
+
+                if self.callbacks.early_stop:
+                    print('Custom Callback Triggered, Process Training Stopped!')
+                    break
+        
+        self.save_final_model()
     
     def validate_one_epoch(self, epoch):
         apcer = npcer = acer = val_loss = 0
